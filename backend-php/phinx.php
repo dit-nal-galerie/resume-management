f24cfd3ab87fe54b80d8fc1e3e7c69f14cfd0e0c
tarno <tarn_news@gmx.de>
2025-10-18 19:33:24 +0200

    anpassen

--- backend-php/phinx.php
+++ backend-php/phinx.php
@@ -1,19 +1,25 @@
+<?php
+
 return [
-'environments' => [
-'default_migration_table' => 'phinxlog',
-'default_environment' => 'testing',
-'testing' => [
-'adapter' => 'mysql',
-'host' => getenv('DB_HOST') ?: '127.0.0.1',
-'name' => getenv('DB_NAME') ?: 'resume_test',
-'user' => getenv('DB_USER') ?: 'root',
-'pass' => getenv('DB_PASS') ?: 'root',
-'port' => 3306,
-'charset' => 'utf8mb4',
-],
-],
-'paths' => [
-'migrations' => 'db/migrations',
-'seeds' => 'db/seeds',
-],
+    'paths' => [
+        'migrations' => 'db/migrations',
+        'seeds' => 'db/seeds',
+    ],
+
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'testing',
+
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => getenv('DB_HOST') ?: '127.0.0.1',
+            'name' => getenv('DB_NAME') ?: 'resume_test',
+            'user' => getenv('DB_USER') ?: 'root',
+            'pass' => getenv('DB_PASS') ?: 'root',
+            'port' => 3306,
+            'charset' => 'utf8mb4',
+        ],
+    ],
+
+    'version_order' => 'creation',
 ];
